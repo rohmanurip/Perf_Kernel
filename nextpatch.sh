8e56346d87979041bbfad9d8a0e8200f16bf8c31
rohmanurip <rohmanurip@gmail.com>
2026-01-23 17:47:10 +0000

    add susfs

---
+++ nextpatch.sh
@@ -0,0 +1,4 @@
+rm -rf KernelSU
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s susfs-rksu-master
+echo "Adding more manager support..."
+echo "Done (KSU, MKSU, RKSU, Fork by backslashxx, SukiSU and WKSU)"

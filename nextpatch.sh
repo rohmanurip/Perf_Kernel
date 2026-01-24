67d7ee51980430bccf5073900636af07f6d91d0b
rohmanurip <rohmanurip@gmail.com>
2026-01-24 06:45:11 +0000

    enable fastcharge

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,4 +1,4 @@
 rm -rf KernelSU
-curl -LSs "https://raw.githubusercontent.com/Impqxr/KernelSU-Next/refs/heads/legacy-susfs/kernel/setup.sh" | bash -s legacy-susfs
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s susfs-rksu-master
 echo "Adding more manager support..."
 echo "Done (KSU, MKSU, RKSU, Fork by backslashxx, SukiSU and WKSU)"

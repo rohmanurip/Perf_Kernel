416d5bb974deffb0346a3a83a92b8b425fc3b2ed
skenakun <ajansyidad2014@gmail.com>
2026-01-26 07:03:05 +0000

    Add support RKSU

---
+++ nextpatch.sh
@@ -0,0 +1,4 @@
+rm -rf KernelSU
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
+echo "Adding more manager support..."
+echo "Done (KSU, MKSU, RKSU, Fork by backslashxx, SukiSU and WKSU)"

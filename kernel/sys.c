30ce87cde85c8d5e1ed56e49dee526dc735fca84
RohmanU <67408676+rohmanurip@users.noreply.github.com>
2026-02-07 02:20:09 +0700

    Update sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1253,16 +1253,19 @@ static int override_release(char __user *release, size_t len)
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
+	uid_t cur_uid = current_uid().val;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (current_uid().val == 0 &&
-		(!strncmp(current->comm, "bpfloader", 9) ||
-		!strncmp(current->comm, "netbpfload", 10) ||
-		!strncmp(current->comm, "netd", 4))) {
-		strcpy(tmp.release, "5.10.23");
-		pr_info("fake uname: %s/%d release=%s\n",
-			 current->comm, current->pid, tmp.release);
+	if (cur_uid == 0) {
+        if (unlikely(!strncmp(current->comm, "bpfloader", 9) ||
+                     !strncmp(current->comm, "netbpfload", 10) ||
+                     !strncmp(current->comm, "netd", 4))) {
+            strlcpy(tmp.release, "5.10.240", sizeof(tmp.release));
+        }
+    }
+	else if (cur_uid >= 1000) {
+        strlcpy(tmp.release, "5.10.240-Perf+", sizeof(tmp.release));
 	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))

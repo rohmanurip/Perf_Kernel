c52b5352328e50ff6dbf517aa1a044495bff40ad
RohmanU <67408676+rohmanurip@users.noreply.github.com>
2026-02-06 13:13:41 +0700

    Update sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1256,6 +1256,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+	if (cur_uid == 0) {
+        if strlcpy(tmp.release, "5.10.240", sizeof(tmp.release));
+    }
+	else if (cur_uid >= 1000) {
+        strlcpy(tmp.release, "5.10.240-Perf+", sizeof(tmp.release));
+	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;

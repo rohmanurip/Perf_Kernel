00755f300867ce9cb8f4eca5bbe1fbfdf5b62c64
RohmanU <67408676+rohmanurip@users.noreply.github.com>
2026-02-06 22:22:19 +0700

    Update version.c

--- fs/proc/version.c
+++ fs/proc/version.c
@@ -8,10 +8,14 @@
 
 static int version_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, linux_proc_banner,
-		utsname()->sysname,
-		utsname()->release,
-		utsname()->version);
+	struct new_utsname *uts = utsname();
+    const char *spoofed_release = "5.10.240-Perf+";
+    
+    seq_printf(m, linux_proc_banner,
+             uts->sysname,
+             spoofed_release,
+             uts->version);
+	
 	return 0;
 }
 

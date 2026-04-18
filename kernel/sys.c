b2c1bbff97cd928819704d7af6e80e7534469791
RohmanU <67408676+rohmanurip@users.noreply.github.com>
2026-04-19 00:36:48 +0700

    Update sys.c to .252

--- kernel/sys.c
+++ kernel/sys.c
@@ -1250,29 +1250,23 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern void susfs_spoof_uname(struct new_utsname* tmp);
-#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
-
 	uid_t cur_uid = current_uid().val;
+
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (cur_uid == 0) {
         if (unlikely(!strncmp(current->comm, "bpfloader", 9) ||
                      !strncmp(current->comm, "netbpfload", 10) ||
                      !strncmp(current->comm, "netd", 4))) {
-            strlcpy(tmp.release, "5.10.240", sizeof(tmp.release));
+            strlcpy(tmp.release, "5.10.252", sizeof(tmp.release));
         }
     }
 	else if (cur_uid >= 1000) {
-        strlcpy(tmp.release, "5.10.240-Perf+", sizeof(tmp.release));
+        strlcpy(tmp.release, "5.10.252-Perf+", sizeof(tmp.release));
 	}
-	#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-		susfs_spoof_uname(&tmp);
-	#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;

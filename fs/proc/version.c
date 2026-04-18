5427c34252903934cf9a752d078aa658b74194ec
RohmanU <67408676+rohmanurip@users.noreply.github.com>
2026-04-19 00:52:29 +0700

    Update version.c to .252

--- fs/proc/version.c
+++ fs/proc/version.c
@@ -9,7 +9,7 @@
 static int version_proc_show(struct seq_file *m, void *v)
 {
 	struct new_utsname *uts = utsname();
-    const char *spoofed_release = "5.10.240-Perf+";
+    const char *spoofed_release = "5.10.252-Perf+";
     
     seq_printf(m, linux_proc_banner,
              uts->sysname,

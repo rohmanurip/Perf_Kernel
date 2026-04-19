37c68b75cc368a6c456dcd098b1420e4d7383c33
RohmanU <67408676+rohmanurip@users.noreply.github.com>
2026-04-19 10:10:39 +0700

    Update spoof 5.10.252

--- kernel/sys.c
+++ kernel/sys.c
@@ -1258,7 +1258,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (cur_uid >= 1000) {
-        strlcpy(tmp.release, "5.10.240-Perf+", sizeof(tmp.release));
+        strlcpy(tmp.release, "5.10.252-Perf+", sizeof(tmp.release));
 	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
